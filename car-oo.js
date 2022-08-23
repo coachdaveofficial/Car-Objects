cdea21cfc43b47755c95c7eed4fdd343759ff0fa
David Falson <david@falson.net>
2022-08-22 18:26:52 -0700

    first commit

---
+++ car-oo.js
@@ -0,0 +1,59 @@
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`
+    }
+    
+    honk() {
+        return "Beep."
+    }
+}
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make,model, year);
+    }
+
+    numWheels = 4;
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make,model, year);
+    }
+
+    numWheels = 2;
+
+    revEngine() {
+        return "VROOM!!!"
+    }
+}
+
+
+
+class Garage {
+    constructor(limit) {
+        this.limit = limit;
+        this.vehicles = [];
+    }
+
+    add(newVehicle) {
+        if (newVehicle instanceof Vehicle !== true) {
+            return "Only vehicles are allowed in here!"
+        }
+        if (this.vehicles.length === this.limit) {
+            return "Sorry, we're full"
+        }
+
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!"
+
+    }
+
+
+}
